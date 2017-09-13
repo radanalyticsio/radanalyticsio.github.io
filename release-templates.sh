c5482b121848fe626fd5a078ac5a4374007b97d8
Trevor McKay <tmckay@redhat.com>
2017-09-13 15:51:32 -0400

    Add a script to generate a resources.yaml referencing tagged images

---
+++ release-templates.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: release-templates.sh VERSION-TAG"
+    exit 1
+fi
+
+TOP_DIR=$(readlink -f `dirname "$0"` | grep -o '.*/radanalyticsio.github.io')
+mkdir -p $TOP_DIR/release_templates
+rm -rf $TOP_DIR/release_templates/*
+
+cp $TOP_DIR/resources.yaml $TOP_DIR/release_templates
+
+sed -r -i "s@(radanalyticsio/radanalytics.*spark)@\1:$1@" $TOP_DIR/release_templates/*
+sed -r -i "s@(radanalyticsio/oshinko-webui)@\1:$1@" $TOP_DIR/release_templates/*
+
+echo "Successfully wrote templates to release_templates/ with version tag $1"
+echo
+echo "grep radanalyticsio/radanalytics.*spark:$1 *"
+echo
+cd $TOP_DIR/; grep radanalyticsio/radanalytics.*spark:$1 release_templates/*
+echo
+echo "grep radanalyticsio/oshinko-webui:$1 *"
+echo
+cd $TOP_DIR/; grep radanalyticsio/oshinko-webui:$1 release_templates/*
+
